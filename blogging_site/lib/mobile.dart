c8cdb79add2ad14895cb5effcef2411f99e108d0
Sagnik Bhattacharya <sagnikbhattachaya1506@gmail.com>
2020-12-22 14:31:36 +0530

    hello
    Signed-off-by: Sagnik Bhattacharya <sagnikbhattachaya1506@gmail.com>

--- blogging_site/lib/mobile.dart
+++ blogging_site/lib/mobile.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'component.dart';
 import 'package:google_fonts/google_fonts.dart';
-//hellos
+//hellosss
 class Mobile extends StatefulWidget {
   @override
   _MobileState createState() => _MobileState();
